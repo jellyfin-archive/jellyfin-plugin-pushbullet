ec1c151d780b898787d60b3293fe9cfc498aa64c
funkimunk <joeraff@gmail.com>
2020-07-28 13:16:03 +0100

    Add files via upload

--- Pushbullet/Configuration/PluginConfiguration.cs
+++ Pushbullet/Configuration/PluginConfiguration.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using MediaBrowser.Model.Plugins;
 
@@ -9,25 +8,53 @@ namespace Pushbullet.Configuration
     /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
-        /// <summary>
-        /// Pushbullet API url.
-        /// </summary>
-        public const string Url = "https://api.pushbullet.com/v2/pushes";
-        private readonly PushbulletOptions[] _options;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
         /// </summary>
         public PluginConfiguration()
         {
-            _options = Array.Empty<PushbulletOptions>();
+            Options = System.Array.Empty<PushbulletOptions>();
         }
 
         /// <summary>
-        /// Get configured options.
+        /// Pushbullet API url.
         /// </summary>
-        /// <returns><see cref="IEnumerable{PushbulletOptions}"/>.</returns>
-        public IEnumerable<PushbulletOptions> GetOptions()
-            => _options;
+
+        public const string Url = "https://api.pushbullet.com/v2/pushes";
+
+        public PushbulletOptions[] Options { get; set; }
+
+    }
+
+    public class PushbulletOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether option is enabled.
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Gets or sets the device id.
+        /// Unused.
+        /// </summary>
+        public string? DeviceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the channel.
+        /// Unused.
+        /// </summary>
+        public string? Channel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user id for this configuration.
+        /// </summary>
+        public string UserId { get; set; } = string.Empty;
+
     }
 }
