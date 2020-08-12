86bb93515c85283767df2a1a5588c5580250150a
David <daullmer@gmail.com>
2020-08-12 14:55:55 +0200

    Migrate to Asp.Net Core Web Api

--- Pushbullet/Api/PushbulletNotificationsController.cs
+++ Pushbullet/Api/PushbulletNotificationsController.cs
@@ -1,58 +1,50 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using MediaBrowser.Common.Net;
 using MediaBrowser.Model.Serialization;
-using MediaBrowser.Model.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Pushbullet.Configuration;
 
 namespace Pushbullet.Api
 {
     /// <summary>
-    /// API endpoints.
+    /// Pushbullet notifications controller.
     /// </summary>
-    public class ServerApiEntryPoints : IService
+    [ApiController]
+    [Route("Notification/Pushbullet")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class PushbulletNotificationsController : ControllerBase
     {
         private readonly IHttpClient _httpClient;
         private readonly IJsonSerializer _jsonSerializer;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ServerApiEntryPoints"/> class.
+        /// Initializes a new instance of the <see cref="PushbulletNotificationsController"/> class.
         /// </summary>
         /// <param name="jsonSerializer">Instance of the <see cref="IJsonSerializer"/> interface.</param>
         /// <param name="httpClient">Instance of the <see cref="IHttpClient"/> interface.</param>
-        public ServerApiEntryPoints(IJsonSerializer jsonSerializer, IHttpClient httpClient)
+        public PushbulletNotificationsController(IJsonSerializer jsonSerializer, IHttpClient httpClient)
         {
             _jsonSerializer = jsonSerializer;
             _httpClient = httpClient;
         }
 
-        private static PushbulletOptions GetOptions(string userId)
+        /// <summary>
+        /// Send Pushbullet test notification.
+        /// </summary>
+        /// <param name="userId">The user id of the Jellyfin user.</param>
+        /// <response code="204">Test notification successfully sent.</response>
+        /// <returns>A <see cref="NoContentResult"/> indicating success.</returns>
+        [HttpPost("Test/{userId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult> PostAsync([FromRoute] string userId)
         {
-            return Plugin.Instance!.Configuration.GetOptions()
+            var options = Plugin.Instance!.Configuration.GetOptions()
                 .FirstOrDefault(i => string.Equals(i.UserId, userId, StringComparison.OrdinalIgnoreCase));
-        }
-
-        /// <summary>
-        /// Send test notification.
-        /// </summary>
-        /// <param name="request">Request to send.</param>
-        public void Post(TestNotification request)
-        {
-            PostAsync(request)
-                .GetAwaiter()
-                .GetResult();
-        }
-
-        /// <summary>
-        /// Send test notification.
-        /// </summary>
-        /// <param name="request">Request to send.</param>
-        /// <returns>A <see cref="Task"/>.</returns>
-        public async Task PostAsync(TestNotification request)
-        {
-            var options = GetOptions(request.UserId!);
 
             var parameters = new Dictionary<string, string>
             {
@@ -71,6 +63,8 @@ namespace Pushbullet.Api
             };
 
             await _httpClient.Post(requestOptions).ConfigureAwait(false);
+
+            return NoContent();
         }
     }
 }
